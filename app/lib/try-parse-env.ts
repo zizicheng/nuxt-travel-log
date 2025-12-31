270ce2124d2b58f589b8c5bf59c9c587a093f313
zizicheng <lttsd@foxmail.com>
2025-12-31 20:32:35 +0800

    :sparkles: typesafe env setup

---
+++ app/lib/try-parse-env.ts
@@ -0,0 +1,28 @@
+/* eslint-disable node/no-process-env */
+
+import type { ZodObject, ZodRawShape } from "zod";
+
+import { ZodError } from "zod";
+
+export default function tryParseEnv<T extends ZodRawShape>(
+  EnvSchema: ZodObject<T>,
+  buildEnv: Record<string, string | undefined> = process.env,
+) {
+  try {
+    EnvSchema.parse(buildEnv);
+  }
+  catch (error) {
+    if (error instanceof ZodError) {
+      let message = "Missing required values in .env:\n";
+      error.issues.forEach((issue) => {
+        message += `${String(issue.path[0])}\n`;
+      });
+      const e = new Error(message);
+      e.stack = "";
+      throw e;
+    }
+    else {
+      console.error(error);
+    }
+  }
+}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
 
+import "./app/lib/env";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
