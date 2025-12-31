3ebd8cb1012861e41ab90a6dbdca4e8d5c8bcf54
zizicheng <lttsd@foxmail.com>
2025-12-31 18:09:13 +0800

    :sparkles: add theme toggle (#51)

--- app/components/app/nav-bar.vue
+++ app/components/app/nav-bar.vue
@@ -6,6 +6,7 @@
       </NuxtLink>
     </div>
     <div class="navbar-end">
+      <AppThemeToggle />
       <a class="btn btn-accent">
         Sign In
         <Icon name="i-tabler:brand-github" size="24" />
