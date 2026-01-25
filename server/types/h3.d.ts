fe7bd8e429f360994dac1b549f5b95fff80eea7a
zizicheng <lttsd@foxmail.com>
2026-01-25 09:50:42 +0800

    :sparkles: implement location fetching API and enhance dashboard with locations display (#63)

--- server/types/h3.d.ts
+++ server/types/h3.d.ts
@@ -1,9 +1,9 @@
-import type { User } from "better-auth";
+import type { UserWithId } from "~/lib/auth";
 
 declare module "h3" {
   // eslint-disable-next-line ts/consistent-type-definitions
   interface H3EventContext {
-    user?: Omit<User, "id"> & { id: number };
+    user?: UserWithId;
   }
 }
 
