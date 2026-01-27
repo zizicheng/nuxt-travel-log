4aef06839a96019a4c21f7a6a37355413500e037
zizicheng <lttsd@foxmail.com>
2026-01-27 22:31:23 +0800

    :sparkles: add map component with MapLibre integration (#65)

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -65,8 +65,9 @@ function toggleSidebar() {
         />
       </div>
     </div>
-    <div class="flex-1">
+    <div class="flex-1 flex flex-col">
       <NuxtPage />
+      <AppMap class="flex-1" />
     </div>
   </div>
 </template>
