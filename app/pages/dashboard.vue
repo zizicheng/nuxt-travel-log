806df1bccdeef4fd45bfb464eee254b9ab7bbda5
zizicheng <lttsd@foxmail.com>
2026-02-03 08:33:13 +0800

    :sparkles: add draggable marker feature and double-click functionality to update location on map

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -81,8 +81,8 @@ function toggleSidebar() {
         />
       </div>
     </div>
-    <div class="flex-1 overflow-auto">
-      <div class="flex flex-col size-full">
+    <div class="flex-1 overflow-auto bg-base-200">
+      <div class="flex size-full" :class="{ 'flex-col': route.path !== '/dashboard/add' }">
         <NuxtPage />
         <AppMap class="flex-1" />
       </div>
